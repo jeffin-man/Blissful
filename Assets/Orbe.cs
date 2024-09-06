e4ca415e9cb4b159c20e01be24184e4055e1d1e7
Vitinho <146314895+Vitieska@users.noreply.github.com>
2024-09-06 16:18:04 -0300

    orbes colidem
    AAAAAEEEEEEEEEEEEEEEEEEPORRAA
    
    Faz 5 horas que eu tento colocar esses orbes pra serem coletados
    
    eu estava quase sendo vencido
    
    mas usei minha ultima carta do baralho:  O Character controller
    
    deu certo

---
+++ Assets/Orbe.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().PegouOrbe();
+            gameObject.SetActive(false);
+        }
+    }
+}
